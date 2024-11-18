8b6923fae13f5ee36d404fdfaeab9d8a91c65cc8
Mubashar hussain <mubashar.hussain@zero-axis.com>
2024-11-18 17:45:53 +0500

    who we serve section updates

--- constants/index.tsx
+++ constants/index.tsx
@@ -153,58 +153,43 @@ export const mobileMenu = [
 			url:'undefined',
 			children:[
 				{
-					title: 'Website Development',
-					url: '/Technology/chatbot-development',
+					title: 'Market Research',
+					url: '/business/market-research/',
 				},
 				{
-					title: 'Chatbot Development',
-					url: '/Technology/chatbot-development',
+					title: 'Operational Assessment',
+					url: '/business/operational-assessment/',
 				},
 				{
-					title: 'Mobile Application Development',
-					url: '/Technology/mobile-app-development',
+					title: 'Professional Business Plans',
+					url: '/business/professional-business-plans/',
 				},
 				{
-					title: 'Website Accessibility',
-					url: '/Technology/website-accessibility',
+					title: 'Business Model Transformation',
+					url: '/business/business-model-transformation/',
+				},
+				{
+					title: 'Event Management',
+					url: '/business/event-management/',
 				},
 			]
 		},
-	{
-		title: 'Market Research',
-		url: 'https://www.primemarketingexperts.com/market-research-services/',
-	},
-	{
-		title: 'Operational Assessment',
-		url: 'https://www.primemarketingexperts.com/operational-assessment/',
-	},
-	{
-		title: 'Professional Business Plans',
-		url: 'https://www.primemarketingexperts.com/professional-business-plans/',
-	},
-	{
-		title: 'Business Model Transformation',
-		url: 'https://www.primemarketingexperts.com/business-model-transformation/',
-	},
-	{
-		title: 'Event Management',
-		url: 'https://www.primemarketingexperts.com/event-management-services/',
-	},
+	
 	{
 		title: 'SEO',
-		url: 'https://www.primemarketingexperts.com/search-engine-optimization/',
+		url: 'undefined',
 		children: [
 			{
 				title: 'Local SEO',
-				url: 'https://www.primemarketingexperts.com/local-seo/',
+				url: '/marketing/local-seo/',
 			},
 			{
 				title: 'SEO Web Design',
-				url: 'https://www.primemarketingexperts.com/seo-web-design/',
+				url: '/marketing/seo-web-design/',
 			},
 			{
 				title: 'Video SEO',
-				url: 'https://www.primemarketingexperts.com/video-seo/',
+				url: '/marketing/video-seo/',
 			},
 		],
 	},
@@ -214,49 +199,53 @@ export const mobileMenu = [
 	},
 	{
 		title: 'Content Marketing',
-		url: 'https://www.primemarketingexperts.com/content-marketing-services/',
+		url: '/marketing/content-marketing/',
 	},
 	{
 		title: 'Search Engine Advertising',
-		url: 'https://www.primemarketingexperts.com/search-engine-advertising/',
+		url: 'undefined',
 		children: [
 			{
 				title: 'Google Shopping Campaigns',
-				url: 'https://www.primemarketingexperts.com/google-shopping-campaign-services/',
+				url: '/marketing/google-shopping-campaigns/',
 			},
 			{
 				title: 'Google Grant Management',
-				url: 'https://www.primemarketingexperts.com/google-grant-management/',
+				url: '/marketing/google-grant-management/',
 			},
 		],
 	},
 	{
 		title: 'Email Marketing',
-		url: 'https://www.primemarketingexperts.com/email-marketing-services/',
+		url: '/marketing/email-marketing/',
 	},
 	{
 		title: 'Branding',
-		url: 'https://www.primemarketingexperts.com/branding/',
+		url: '/marketing/branding/',
+	},
+	{
+		title: 'Social Media Marketing',
+		url: '/marketing/social-media-marketing/',
 	},
 	{
 		title: 'Text Message Marketing',
-		url: 'https://www.primemarketingexperts.com/text-message-marketing/',
+		url: '/marketing/text-message-marketing/',
 	},
 	{
 		title: 'Marketing Automation',
-		url: 'https://www.primemarketingexperts.com/marketing-automation-services/',
+		url: '/marketing/marketing-automation/',
 	},
 	{
 		title: 'Marketing Plans',
-		url: 'https://www.primemarketingexperts.com/marketing-plan-services/',
+		url: '/marketing/marketing-plans/',
 	},
 	{
 		title: 'Inbound Marketing',
-		url: 'https://www.primemarketingexperts.com/inbound-marketing-services/',
+		url: '/marketing/inbound-marketing-services/',
 	},
 	{
 		title: 'E-Commerce Services',
-		url: 'https://www.primemarketingexperts.com/e-commerce-digital-marketing-services/',
+		url: '/marketing/e-commerce-digital-marketing-services/',
 	},
 
 	{
