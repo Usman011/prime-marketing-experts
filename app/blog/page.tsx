03798dc21b3bb98d9812034d1da11f819166d359
Mubashar Hussain <mubashar.hussain@zero-axis.com>
2024-12-04 04:50:20 -0800

    updates blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Page = () => {
 	return (
-		<section className='container mx-auto px-2'>
+		<section className='container mx-auto mb-10 px-2'>
      
 		{/* Header */}
 	 <div className='flex justify-center sm:items-center lg:items-center md:pl-10 sm:pl-20 lg:ml-auto w-full h-28 flex-col text-center'>
